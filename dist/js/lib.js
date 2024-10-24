228a711844a13b166567d402dbcc12d911f56142
srg <skabassjob@gmail.com>
2024-10-24 17:55:04 +0400

    feat: build update

--- dist/js/lib.js
+++ dist/js/lib.js
@@ -46,7 +46,7 @@ window.utils = {
           forceToAxis: true
         },
         initialSlide,
-        spaceBetween: 16,
+        spaceBetween: 12,
         grabCursor: true,
         slidesPerView: 'auto',
         freeMode: {
@@ -59,11 +59,70 @@ window.utils = {
           nextEl: nextEl
         },
         breakpoints: {
-          540: {
+          768: {
             spaceBetween: 30
           }
         }
       })
+    })
+  })
+})()
+
+/**
+ * f-select.js
+ */
+;(() => {
+  /** @type HTMLDivElement[]*/
+  const selects = document.querySelectorAll('.js-select')
+
+  selects.forEach((select) => {
+    /** @type HTMLInputElement | null */
+    const field = select.querySelector('.js-select-input')
+
+    /** @type HTMLButtonElement | null */
+    const btn = select.querySelector('.js-select-btn')
+
+    /** @type HTMLSpanElement | null */
+    const btnText = select.querySelector('.js-select-value')
+
+    /** @type HTMLDivElement | null */
+    const drop = select.querySelector('.js-select-drop')
+
+    /** @type HTMLButtonElement[] */
+    const items = select.querySelectorAll('.js-select-item')
+
+    if (!field || !btn || !btnText || !drop || items.length === 0) {
+      return
+    }
+    console.log('test1')
+
+    btn.addEventListener('click', () => {
+      select.classList.toggle('show')
+    })
+
+    drop.addEventListener('click', (evt) => {
+      if (!evt.target) {
+        return
+      }
+
+      if (evt.target.tagName !== 'BUTTON') {
+        return
+      }
+
+      items.forEach((item) => item.classList.remove('actv'))
+      evt.target.classList.add('actv')
+      select.classList.remove('show')
+
+      const value = evt.target.dataset.inputValue
+
+      if (value) {
+        field.value = value
+        btnText.textContent = evt.target.textContent
+        select.classList.remove('empty')
+      } else {
+        field.removeAttribute('value')
+        select.classList.add('empty')
+      }
     })
   })
 })()
